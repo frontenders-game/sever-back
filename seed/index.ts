d9190e1faac90dd147b81ed58a0cf095584d5934
frontenders_game <sounnecessary83@gmail.com>
2023-03-28 06:29:23 +0300

    added seed data

--- seed/index.ts
+++ seed/index.ts
@@ -21,52 +21,48 @@ const readJson = (jsonPath: string) => {
 const readCategory = (categoryDir: string): CreateCategoryInput => readJson(`${categoryDir}/category.json`)
 const readSubcategory = (subcategoryDir: string): CreateSubcategoryInput => readJson(`${subcategoryDir}/subcategory.json`)
 
-// const readProduct = (categoryId, subcategoryId) =>
-//     readJson(`data/${categoryId}/${subcategoryId}/subcategory.json`)
-
 async function addCategoryToDb(categoryInput: CreateCategoryInput): Promise<Category> {
-    const dbCategory: Category = await createCategoryService(categoryInput)
-    return dbCategory
+    return await createCategoryService(categoryInput)
 }
 
 async function addSubcategoryToDb(categoryInput: CreateSubcategoryInput): Promise<Subcategory> {
-    const dbSubcategory: Subcategory = await createSubcategoryService(categoryInput)
-    return dbSubcategory
+    return await createSubcategoryService(categoryInput)
 }
 
 async function addProductToDb(productInput: CreateProductInput): Promise<Product> {
-    const dbProduct: Product = await createProductService(productInput)
-    return dbProduct
+    return await createProductService(productInput)
 }
 
 async function addImageToProduct(productPath: string, productId: string): Promise<ProducImage> {
     const imageData: Buffer = fs.readFileSync(productPath)
     const imageResizer = new ImageResizer(imageData, productId, 1)
     const imagePaths = await imageResizer.saveAll()
-    const image = await createProductImageService({
+    return await createProductImageService({
         productId: productId,
         order: 1,
         ...imagePaths
     })
-    return image
 }
 
 async function walkSeedData(dataDir: string) {
     const categories = readDir(dataDir);
     // iter categories
-    for (const categoryId of categories) {
-        const categoryDir = path.join(dataDir, categoryId.name)
+    for (const categoryFolder of categories) {
+        const categoryDir = path.join(dataDir, categoryFolder.name)
         const category = readCategory(categoryDir)
-        console.log('Working on Category: ', category.name)
+        console.log('Working on Category: ', category.name, 'id ', categoryFolder.name)
         const addedCategory: Category = await addCategoryToDb(
             {name: category.name, description: `Категория: ${category.name}`})
         const subcategories = readDir(categoryDir)
         // iter subcategories
-        for (const subcategoryId of subcategories) {
-            if (subcategoryId.isDirectory()) {  // avoid category.json
-                const subcategoryDir = path.join(categoryDir, subcategoryId.name)
+        for (const subcategoryFolder of subcategories) {
+            if (subcategoryFolder.isDirectory()) {  // avoid category.json
+                const subcategoryDir = path.join(categoryDir, subcategoryFolder.name)
+                const productsJsonDir = path.join(subcategoryDir, 'products/json')
+                const productsImageDir = path.join(subcategoryDir, 'products/images')
+                if (!fs.existsSync(productsJsonDir)) continue  // no products
                 const subcategory: CreateSubcategoryInput = readSubcategory(subcategoryDir)
-                console.log('Working on Subcategory: ', subcategory)
+                console.log('Working on Subcategory: ', subcategory.name, 'id ', subcategoryFolder.name)
                 const addedSubcategory: Subcategory =
                     await addSubcategoryToDb({
                         name: subcategory.name,
@@ -74,22 +70,17 @@ async function walkSeedData(dataDir: string) {
                         categoryId: addedCategory.id
                     })
                 console.log(addedSubcategory.id)
-                const productsJsonDir = path.join(subcategoryDir, 'products/json')
-                const productsImageDir = path.join(subcategoryDir, 'products/images')
-                if (!fs.existsSync(productsJsonDir)) continue  // no products
                 const productJsons = readDir(productsJsonDir)
                 for (const productJsonFile of productJsons) {
-                    if (productJsonFile.name.endsWith('json')) {
-                        const product: CreateProductInput =
-                            readJson(path.join(productsJsonDir, productJsonFile.name))
-                        const addedProduct =
-                            await addProductToDb({...product, subcategoryId: addedSubcategory.id})
-                        console.log('added product:', addedProduct.id, product.name)
-                        const productImagePath = path.join(productsImageDir, productJsonFile.name.replace('.json', ''), '1.jpg')
-                        const addedProductImage = await addImageToProduct(productImagePath, addedProduct.id)
-                        console.log(`added image: ${addedProductImage.id} for product ${addedProduct.id}`)
-
-                    }
+                    if (!productJsonFile.name.endsWith('json')) continue
+                    const product: CreateProductInput =
+                        readJson(path.join(productsJsonDir, productJsonFile.name))
+                    const addedProduct =
+                        await addProductToDb({...product, subcategoryId: addedSubcategory.id})
+                    console.log('added product:', addedProduct.id, product.name)
+                    const productImagePath = path.join(productsImageDir, productJsonFile.name.replace('.json', ''), '1.jpg')
+                    const addedProductImage = await addImageToProduct(productImagePath, addedProduct.id)
+                    console.log(`added image: ${addedProductImage.id} for product ${addedProduct.id}`)
                 }
             }
         }
